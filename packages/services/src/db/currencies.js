a12b43373e73925ae72afbfa438821457f17f9c0
Aaron Lo <aaronlo207@gmail.com>
2019-06-05 20:01:05 -0400

    removed files

--- packages/services/src/db/currencies.js
+++
@@ -1,7 +0,0 @@
-import { model, Schema } from "mongoose";
-const currencySchema = {
-    tier: Number,
-    currency: String
-};
-const CurrencySchema = new Schema(currencySchema);
-export const Currency = model("egg_currencies", CurrencySchema);
--- packages/services/src/db/setup.js
+++
@@ -1,3 +0,0 @@
-import mongoose from "mongoose";
-mongoose.connect(process.env.MONGO_URL).then(() => console.log("connected!"));
-import("fs").then(fs => fs.promises.read);
